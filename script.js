f3c4f7b01bb51a6fe51a29a7f186ffee6daa8519
juamoras <105953028+juamoras@users.noreply.github.com>
2023-05-05 21:17:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,4 +21,34 @@ document.getElementById("start-button").addEventListener("click", function() {
   // Generate lioness
   lioness = createLioness();
   document.getElementById("gender").textContent = lioness.gender;
-  document.getElementById("
+  document.getElementById("appearance").textContent = lioness.appearance;
+  document.getElementById("age").textContent = lioness.age;
+  document.getElementById("pride-name").textContent = lioness.pride.name;
+  document.getElementById("pride-members").textContent = lioness.pride.members;
+  document.querySelector(".start-screen").style.display = "none";
+  document.querySelector(".game").style.display = "grid";
+});
+
+document.getElementById("next-button").addEventListener("click", function() {
+  const eventIndex = Math.floor(Math.random() * events.length);
+  const event = events[eventIndex];
+  document.getElementById("event").textContent = event;
+  lioness.age += 1;
+  document.getElementById("age").textContent = lioness.age;
+});
+
+function createLioness() {
+  const newLioness = Object.assign({}, lioness);
+  newLioness.age = 0;
+  newLioness.name = generateName();
+  newLioness.appearance = newLioness.appearance[Math.floor(Math.random() * newLioness.appearance.length)];
+  return newLioness;
+}
+
+function generateName() {
+  const prefixes = ["As", "Am", "Is", "Ab", "Mak", "Nal", "Niz", "Sar", "Zar", "Zar"];
+  const suffixes = ["ra", "ma", "ia", "an", "la", "ni", "zi", "ya", "ba", "da", "na", "ya", "sa", "ta", "ka"];
+  const prefix = prefixes[Math.floor(Math.random() * prefixes.length)];
+  const suffix = suffixes[Math.floor(Math.random() * suffixes.length)];
+  return prefix + suffix;
+}
