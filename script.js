f597fba8c3ca0f97c9b844c15bfd630cb869b564
juamoras <105953028+juamoras@users.noreply.github.com>
2023-05-05 21:36:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,52 +6,46 @@ const events = [
   // Add more events here
 ];
 
-let lioness = {
-  gender: "female",
-  appearance: ["golden", "sandy", "dark", "light", "spotted", "striped", "solid"],
-  age: 0,
-  name: "",
-  pride: {
-    name: "Pridelands",
-    members: 20
-  }
-};
-
-document.getElementById("start-link").addEventListener("click", function(event) {
-  event.preventDefault(); // Prevent default link behavior
-  document.querySelector(".start-screen").classList.add("hidden"); // Hide start screen
-  document.querySelector(".game").classList.remove("hidden"); // Show game screen
-  lioness = createLioness(); // Generate lioness
-  document.getElementById("gender").textContent = lioness.gender;
-  document.getElementById("appearance").textContent = lioness.appearance;
-  document.getElementById("age").textContent = lioness.age;
-  document.getElementById("pride-name").textContent = lioness.pride.name;
-  document.getElementById("pride-members").textContent = lioness.pride.members;
-  // Display first event
-  document.getElementById("event").textContent = events[0];
-});
-
+let lioness = createLioness();
+document.getElementById("gender").textContent = lioness.gender;
+document.getElementById("appearance").textContent = lioness.appearance;
+document.getElementById("age").textContent = lioness.age;
+document.getElementById("pride-name").textContent = lioness.pride.name;
+document.getElementById("pride-members").textContent = lioness.pride.members;
 
 document.getElementById("next-button").addEventListener("click", function() {
+  // Generate event
   const eventIndex = Math.floor(Math.random() * events.length);
   const event = events[eventIndex];
   document.getElementById("event").textContent = event;
-  lioness.age += 1;
-  document.getElementById("age").textContent = lioness.age;
+
+  // Update lioness age
+  lioness.age++;
+
+  // TODO: Add more game logic here
 });
 
 function createLioness() {
-  const newLioness = Object.assign({}, lioness);
-  newLioness.age = 0;
-  newLioness.name = generateName();
-  newLioness.appearance = newLioness.appearance[Math.floor(Math.random() * newLioness.appearance.length)];
-  return newLioness;
+  const lioness = {
+    gender: "female",
+    appearance: ["golden", "sandy", "dark", "light", "spotted", "striped", "solid"][Math.floor(Math.random() * 7)],
+    age: 0,
+    name: generateName()
+    pride: {
+      name: "Pridelands",
+      members: 20
+    }
+  };
+  
+  function generateName() {
+  const prefixes = ["Ki", "Ku", "Tu", "Ta", "Za", "Nya", "Ma", "Sha", "Aku", "Mala"];
+  const suffixes = ["ra", "ni", "sha", "ki", "la", "si", "na", "ma", "li", "ku"];
+  const prefixIndex = Math.floor(Math.random() * prefixes.length);
+  const suffixIndex = Math.floor(Math.random() * suffixes.length);
+  const name = prefixes[prefixIndex] + suffixes[suffixIndex];
+  return name;
 }
 
-function generateName() {
-  const prefixes = ["As", "Am", "Is", "Ab", "Mak", "Nal", "Niz", "Sar", "Zar", "Zar"];
-  const suffixes = ["ra", "ma", "ia", "an", "la", "ni", "zi", "ya", "ba", "da", "na", "ya", "sa", "ta", "ka"];
-  const prefix = prefixes[Math.floor(Math.random() * prefixes.length)];
-  const suffix = suffixes[Math.floor(Math.random() * suffixes.length)];
-  return prefix + suffix;
+
+  return lioness;
 }
